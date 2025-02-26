3b84b5ab0064f101907f7109ebf150cdf276d432
Jason <jason974448@gmail.com>
2025-02-26 19:59:47 -0400

    finished running animation

---
+++ Assets/Scripts/HumanScrollWheal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanScrollWheal : MonoBehaviour
+{
+    Animator Human;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Human = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
